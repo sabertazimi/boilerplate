f51cda00c54daa6fd0ceee25473784f1fbcd88d5
sabertazimi <sabertazimi@gmail.com>
2019-03-02 00:36:12 +0800

    update(webpack-loader): font/file/image loader

--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,21 @@ module.exports = {
         test: /\.(css|scss)$/,
         use: [...styleLoader],
       },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?name=images/[name].[ext]',
+          'image-webpack-loader?bypassOnDebug',
+        ],
+      },
     ],
   },
   plugins: [
